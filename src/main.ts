33985df518a7762e95332519bb9c7d1ac2371492
Lin Huang <lhuangufl@gmail.com>
2024-02-23 11:24:00 -0500

    Missed files in previous commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { SIDEBAR_ROUTE } from './app/sidebar/sidebar.route';
+import { APP_ROUTE } from './app/app.route';
 
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(SIDEBAR_ROUTE)],
+}).catch(err => console.error(err));
