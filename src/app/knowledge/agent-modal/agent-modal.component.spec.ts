52b1586026d383fd4f7348facf49e214fdd813d9
Lin Huang <lhuangufl@gmail.com>
2024-02-27 08:11:59 -0500

    move to bank

---
+++ src/app/knowledge/agent-modal/agent-modal.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AgentModalComponent } from './agent-modal.component';
+
+describe('AgentModalComponent', () => {
+  let component: AgentModalComponent;
+  let fixture: ComponentFixture<AgentModalComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AgentModalComponent]
+    });
+    fixture = TestBed.createComponent(AgentModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/knowledge/knowledge.component.ts
+++ src/app/knowledge/knowledge.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Component, Output, EventEmitter, inject, ApplicationRef, Injector, OnDestroy } from '@angular/core';
+import { AgentComponent } from '../hub/agent/agent.component';
+import { Agent } from '../hub/hubAgent';
+import { HubService } from '../hub/hub.service';
+import { AgentModalComponent } from './agent-modal/agent-modal.component';
 @Component({
   selector: 'app-knowledge',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AgentComponent, AgentModalComponent],
   templateUrl: './knowledge.component.html',
   styleUrls: ['./knowledge.component.css']
 })
 export class KnowledgeComponent {
+  activeTab: string = 'knowledgeBases';
+  agentList: Agent[] = [];
+  isModalVisible: boolean = false;
 
+  hubService: HubService = inject(HubService);
+  constructor() {
+    this.agentList = this.hubService.getAllAgents();
+  }
+  showModal(): void {
+    this.isModalVisible = true;
+  }
+  @Output() close = new EventEmitter<void>(); // This emits an event when the modal should close
+
+  closeModal(): void {
+    this.isModalVisible = false;
+    this.close.emit(); // Notify the parent component to close the modal
+  }
+  addNewAgent(newAgent: Agent): void {
+    // You might want to assign a unique ID for the new agent
+    newAgent.id = this.agentList.length + 1;
+    this.agentList.push(newAgent);
+    this.closeModal();
+  }
 }
